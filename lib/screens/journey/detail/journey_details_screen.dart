6e544c6a5a1f2d440e91e2ba271ae00c3af4f951
Paul Breton <pbreton@betomorrow.com>
2020-08-17 16:35:19 +0200

    Added image to details.

--- lib/screens/journey/detail/journey_details_screen.dart
+++ lib/screens/journey/detail/journey_details_screen.dart
@@ -1,5 +1,7 @@
 import 'package:dev/models/dtos/journey.dart';
+import 'package:dev/models/dtos/picture.dart';
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class JourneyDetailsScreen extends StatelessWidget {
   const JourneyDetailsScreen({Key key}) : super(key: key);
@@ -10,22 +12,45 @@ class JourneyDetailsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
+        toolbarHeight: 111,
+        leadingWidth: 100,
         elevation: 0,
-        leading: InkWell(
-          onTap: () => Navigator.of(context).pop(),
-          customBorder: CircleBorder(),
-          child: Container(
-            margin: EdgeInsets.only(left: 30, top: 30),
-            width: 44,
-            height: 44,
-            decoration:
-                BoxDecoration(shape: BoxShape.circle, color: Color.fromRGBO(27, 33, 15, 0.14)),
-            child: Icon(Icons.arrow_back),
-          ),
+        leading: Container(
+          margin: EdgeInsets.only(left: 30, top: 30),
+          width: 44,
+          height: 44,
+          decoration:
+              BoxDecoration(shape: BoxShape.circle, color: Color.fromRGBO(27, 33, 15, 0.14)),
+          child: InkWell(
+              onTap: () => Navigator.of(context).pop(),
+              customBorder: CircleBorder(),
+              child: const Icon(Icons.arrow_back, size: 40)),
         ),
       ),
       extendBodyBehindAppBar: true,
-      body: Container(color: Colors.blue, height: 400, child: Text(journey.title.fr)),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 362,
+            child: _buildImage(journey.pictures[0]),
+          )
+        ],
+      ),
     );
+  }
+
+  _buildImage(Picture image) {
+    if (image == null) {
+      return Image.asset(
+        "assets/images/placeholder.jpeg",
+        fit: BoxFit.cover,
+      );
+    } else {
+      return FadeInImage.memoryNetwork(
+        placeholder: kTransparentImage,
+        image: image.src,
+        fit: BoxFit.cover,
+      );
+    }
   }
 }
